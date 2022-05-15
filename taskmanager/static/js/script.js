5d2c7a903d106c0fcc2110323346bfe60d49e451
jdj0 <joshua.davidjones92@gmail.com>
2022-05-15 14:51:55 +0000

    Create form to submit new tasks to the database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
      M.Sidenav.init(sidenav);
-  });
+
+     // datepicker initialization    
+     let datepicker = document.querySelectorAll('.datepicker');
+     M.Datepicker.init(datepicker, {
+         format: "dd mmmm, yyyy"
+     });
+     
+     // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+});
